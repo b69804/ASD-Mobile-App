f1ee3e1274db77b38010aba7e2820df1afcc3391
Matthew Ashton <mashton237@fullsail.com>
2013-09-12 19:00:17 -0400

    numerous fixes to delete and edit

--- js/main.js
+++ js/main.js
@@ -15,33 +15,27 @@ $("#addGame").on("pageinit", function(){
                 submitHandler: function() {
                         var data = myForm.serializeArray();
 			storeData(data);
+			window.location.reload();
 			
                 }
     });
 
-// $("#submit").on("click", function(){
-        //if ($('#key').val() == "") {
-          //  var key = Math.floor(Math.random()*10000001);
-        //} else {
-          //  var key = $("#key").val();
-        //}
-        //var opponet = $("#opponet").val();
-        //var dateOfGame = $("#dateOfGame").val();
-        //var homeAway = $("#homeAway").val();
-        //var competition = $("#competition").val();
-        //var mustWatch = $("mustWatch").val();
-        //var prediction = $("#prediction").val();
-        
-          //  $(".display ul").append('<li><a href="#" data-key="' + key + '" class="edit">' + opponet + "" + dateOfGame + "" + homeAway + "" + competition + "" + mustWatch + "" + prediction + '</a></li>');
-            //$(".edit").on("click",function(){
-              // var myKey = $(this).data("key");
-                //})
-        //}); 
-var storeData = function(key){
-                if (!key) {
+    $("#clearGames").on('click', function(){
+        if(localStorage.length === 0) {
+            alert("No Games are Available.");
+        }else{
+            localStorage.clear();
+            alert("Games have been cleared.");
+            window.location.reload();
+            return false;
+        }
+    });
+
+var storeData = function(key, value){
+            if ($('#key').val() == '') {
                    var ID      = Math.floor(Math.random()*10000001);
                  }else{
-                    ID = key;
+		    var ID = $("#key").val();
         }
                 var item    = {};
                 item.opponet       =["Opponet", $("#opponet").val()];
@@ -50,10 +44,12 @@ var storeData = function(key){
                 item.competition          =["Competition:", $("#competition").val()];
                 item.mustWatch          =["Is This a Must Watch Game?:", $("#mustWatch").val()];
                 item.prediction       =["Prediction:", $("#prediction").val()];
-            
+		
         localStorage.setItem(ID, JSON.stringify(item));
         alert("Game Saved!");
-	location.reload();  
+	var ID= key;
+	window.location.reload();
+	  
     };
 });
 
@@ -65,19 +61,43 @@ $("#browseGames").on("pageinit", function(){
             var key = localStorage.key(i);
             var item = JSON.parse(localStorage.getItem(key));
             var makeSubList = $("<li></li>");
-            var makeSubLi = $( "<h3>"+item.opponet[1]+"</h3>"+
-                "<p><strong>"+item.dateOfGame[1]+"</strong></p>"+
-                "<p>"+item.homeAway[1]+"</p>" +
-                "<p>"+item.competition[1]+"</p>" +
-		"<p>"+item.mustWatch[1]+"</p>" +
-		"<p>"+item.prediction[1]+"</p>")
-		.appendTo("#gameList");
-            //var makeLink = $("<a href='#' id='"+key+"'>Edit</a>");
-            //makeLink.on('click', function(){});
-            //makeLink.html(makeSubLi);
-            //makeSubList.append(makeLink).appendTo("#gameList");
-        }; 
-        //$("gameList").listview('refresh');
-	location.reload();
-    });    
+            var makeSubLi = $( "<h3>Opponet:  "+item.opponet[1]+"</h3>"+
+                "<p><strong> Date of Game:  "+item.dateOfGame[1]+"</strong></p>"+
+                "<p>Is MUFC Home or Away?  "+item.homeAway[1]+"</p>" +
+                "<p>Competition:  "+item.competition[1]+"</p>" +
+		"<p>Is this a Must Watch Game?  "+item.mustWatch[1]+"</p>" +
+		"<p>Match Prediction:  "+item.prediction[1]+"</p>" +
+		"<a href='#addGame' data-role='button' id='"+ key +"'>Edit</a>" + "<br>" +
+		"<a href='#' data-role='button' id='"+ key +"' class='deleteGame'>Delete</a>")
+            //var makeLink = $("<a href='#addGame' data-role='button' id='"+ key +"'>Edit</a>");
+            //makeLink.prepend(makeSubLi);
+	   // makeLink.on('click', function(editGame){});
+	    $('+ key +').on('click', function(){});
+	    
+            makeSubLi.appendTo("#gameList");
+	    
+	    $(".deleteGame").on('click', function(){
+		localStorage.removeItem(key);
+		alert("Game Deleted");
+		window.location.reload();
+	    });
+	    
+	    
+	    
+	    //var ask = alert("Are you absolutely sure you want to delete this game?");
+		  //  if (ask) {
+		//	localStorage.removeItem('+ key +');
+		//	window.location.reload();
+		  //  }else{
+		//	alert("Game was NOT deleted!");
+		//    }
+	    //});	     
+        };
+
+        $("gameList").listview('refresh');
+	
+	
+    });
+    
+    
 });
