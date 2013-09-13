d293ac7391b1410675a0387e05cc77a047654be2
Matthew Ashton <mashton237@fullsail.com>
2013-09-12 22:33:33 -0400

    JSON and XML now work!

--- js/main.js
+++ js/main.js
@@ -55,43 +55,78 @@ var storeData = function(key, value){
 
 $("#browseGames").on("pageinit", function(){
     
+    $("#loadJSON").on('click', function(){
+	$.ajax({
+	    url: "xhr/data.json",
+	    type: "GET",
+	    dataType: "json",
+	    success: function(response){
+		for (var i= 0, j=response.items.length; i<j ; i++){
+		    var item = response.items[i];
+		    $( "<h3>Opponet:  "+item.opponet+"</h3>"+
+                "<p><strong> Date of Game:  "+item.dateOfGame+"</strong></p>"+
+                "<p>Is MUFC Home or Away?  "+item.homeAway+"</p>" +
+                "<p>Competition:  "+item.competition+"</p>" +
+		"<p>Is this a Must Watch Game?  "+item.mustWatch+"</p>" +
+		"<p>Match Prediction:  "+item.prediction+"</p>").appendTo('#gameList').trigger("create");
+		};
+	    }
+	});
+    });
+    
+    $("#loadXML").on('click', function(){
+	$.ajax({
+	    url: "xhr/data.xml",
+	    type: "GET",
+	    dataType: "xml",
+	    success: function(xml){
+		var items = $(xml);
+		items.find("item").each(function(){
+		    var item = $(this);
+		    $( "<li>" + "<h3>Opponet:  "+item.find("opponet").text()+"</h3>"+
+		    "<p><strong> Date of Game:  "+item.find("dateOfGame").text()+"</strong></p>"+
+		    "<p>Is MUFC Home or Away?  "+item.find("homeAway").text()+"</p>" +
+		    "<p>Competition:  "+item.find("competition").text()+"</p>" +
+		    "<p>Is this a Must Watch Game?  "+item.find("mustWatch").text()+"</p>" +
+		    "<p>Match Prediction:  "+item.find("prediction").text()+"</p>" + "</li>").appendTo('#gameList').trigger("create");
+		});
+	    }	
+	});
+    });
+    
     $("#localStorage").on('click', function(){
         $("#gameList").empty();
         for (var i= 0, j=localStorage.length; i<j ; i++){
             var key = localStorage.key(i);
             var item = JSON.parse(localStorage.getItem(key));
             var makeSubList = $("<li></li>");
-            var makeSubLi = $( "<h3>Opponet:  "+item.opponet[1]+"</h3>"+
+            var makeSubLi = $( "<li>" + "<h3>Opponet:  "+item.opponet[1]+"</h3>"+
                 "<p><strong> Date of Game:  "+item.dateOfGame[1]+"</strong></p>"+
                 "<p>Is MUFC Home or Away?  "+item.homeAway[1]+"</p>" +
                 "<p>Competition:  "+item.competition[1]+"</p>" +
 		"<p>Is this a Must Watch Game?  "+item.mustWatch[1]+"</p>" +
 		"<p>Match Prediction:  "+item.prediction[1]+"</p>" +
-		"<a href='#addGame' data-role='button' id='"+ key +"'>Edit</a>" + "<br>" +
-		"<a href='#' data-role='button' id='"+ key +"' class='deleteGame'>Delete</a>")
-            //var makeLink = $("<a href='#addGame' data-role='button' id='"+ key +"'>Edit</a>");
-            //makeLink.prepend(makeSubLi);
-	   // makeLink.on('click', function(editGame){});
-	    $('+ key +').on('click', function(){});
-	    
-            makeSubLi.appendTo("#gameList");
-	    
+		"<a href='#addGame' data-role='button' id='"+ key +"' class='editGame'>Edit Game</a>" +
+		"<a href='#' data-role='button' id='"+ key +"' class='deleteGame'>Delete Game</a>" + "</li>")
+	    makeSubLi.addClass('newList');
+            makeSubLi.appendTo("#gameList").trigger("create");
+	    $('.editGame').on('click', function(){
+		var editGameEntry = this.key;
+		$('#opponet').val(item.opponet[1]);
+		$('#dateOfGame').val(item.dateOfGame[1]);
+		$('#homeAway').val(item.homeAway[1]);
+		$('#competition').val(item.competition[1]);
+		$('#mustWatch').val(item.mustWatch[1]);
+		$('#prediction').val(item.prediction[1]);
+		
+		
+	    });
 	    $(".deleteGame").on('click', function(){
+		alert("Are you sure you want to do that?");
 		localStorage.removeItem(key);
 		alert("Game Deleted");
 		window.location.reload();
-	    });
-	    
-	    
-	    
-	    //var ask = alert("Are you absolutely sure you want to delete this game?");
-		  //  if (ask) {
-		//	localStorage.removeItem('+ key +');
-		//	window.location.reload();
-		  //  }else{
-		//	alert("Game was NOT deleted!");
-		//    }
-	    //});	     
+	    });	     
         };
 
         $("gameList").listview('refresh');
